3fca8c0fabf6e292a1e8b42586f5aebc2101a316
eseceve <eseceve@gmail.com>
2016-10-21 00:15:34 -0300

    Fix hour and remove days

--- scripts.js
+++ scripts.js
@@ -12,13 +12,11 @@
   }
 
   function updateTimer() {
-    var day = document.getElementById('timerDay');
     var hour = document.getElementById('timerHour');
     var minute = document.getElementById('timerMinute');
     var second = document.getElementById('timerSecond');
-    var data = getTimeRemaining('2016-10-21T20:00:00');
+    var data = getTimeRemaining('2016-10-21 20:00:00 -0300');
 
-    day.innerHTML = data.days;
     hour.innerHTML = ('0' + data.hours).slice(-2);
     minute.innerHTML = ('0' + data.minutes).slice(-2);
     second.innerHTML = ('0' + data.seconds).slice(-2);
@@ -29,10 +27,8 @@
     var seconds = Math.floor((time / 1000) % 60);
     var minutes = Math.floor((time / 1000 / 60) % 60);
     var hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(time / (1000 * 60 * 60 * 24));
 
     return {
-      'days': days,
       'hours': hours,
       'minutes': minutes,
       'seconds': seconds
